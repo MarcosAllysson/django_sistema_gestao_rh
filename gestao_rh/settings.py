7d22fb1196159ab120a63827cfb518ca02b8f50a
MarcosAllysson <marcosgracip05@gmail.com>
2021-07-26 11:13:26 -0300

    app funcionarios

--- gestao_rh/settings.py
+++ gestao_rh/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'empresas.apps.EmpresasConfig',
+    'funcionarios.apps.FuncionariosConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
