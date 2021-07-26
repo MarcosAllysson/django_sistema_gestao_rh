8dca1cc325730a9d99c5515accfc1cf96109c3ea
MarcosAllysson <marcosgracip05@gmail.com>
2021-07-26 11:17:57 -0300

    app departamento

--- gestao_rh/settings.py
+++ gestao_rh/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'empresas.apps.EmpresasConfig',
     'funcionarios.apps.FuncionariosConfig',
+    'departamentos.apps.DepartamentosConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
